18d2d09d1d4aa7dbf97165a7720e8cd0c8eadff8
jujoo <wnfks93@gmail.com>
2014-09-11 23:52:49 +0900

    cow walk

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
+var cow;
 
 function preload(){
-	game.load.image('cow', 'assets/cow.png');
+	game.load.spritesheet('cow', 'assets/cow.png', 100, 50);
 }
+
 function create(){
 	game.add.sprite(100,100, 'cow');
+	cow.animation.add('walk');
+	cow.animations.play('walk', 10, true);
 }
