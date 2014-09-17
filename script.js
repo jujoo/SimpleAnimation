da62ac25fb4966bbb3c4cc372b618122648eb29d
jujoo <wnfks93@gmail.com>
2014-09-17 17:52:42 +0900

    background

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
 var cow;
+var bg;
 
 function preload(){
 	game.load.spritesheet('cow', 'assets/cow.png', 100, 50);
+	game.load.image('bg', 'assets/bg.jpg');
 }
 
 function create(){
+	bg= game.add.sprite(0,0, 'bg');
+
 	cow = game.add.sprite(100,100, 'cow');
 	cow.animations.add('walk');
 	cow.inputEnabled = true;
