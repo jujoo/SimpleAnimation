9676c36e9af77d42eaf0cde7ae915b9a2181200a
jujoo <wnfks93@gmail.com>
2014-09-17 19:09:06 +0900

    추가 : 가로등, 풍차

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
 var bg;
 var cow;
-var cow2
-
+var cow2;
+var streetlamp;
+var windmill;
 
 function preload(){
 	game.load.spritesheet('cow', 'assets/cow.png', 100, 50);
 	game.load.spritesheet('cow2', 'assets/cow2.png', 84, 45);
+	game.load.spritesheet('streetlamp', 'assets/streetlamp.png', 39, 204);
+	game.load.spritesheet('windmill', 'assets/windmill.png', 176, 201);
+
 	game.load.image('bg', 'assets/bg.jpg');
 }
 
@@ -19,6 +23,12 @@ function create(){
 	cow.events.onInputDown.add(cowClick, this);
 
 	cow2 = game.add.sprite(200,100, 'cow2');
+
+	streetlamp = game.add.sprite(300,100, 'streetlamp');
+
+	windmill = game.add.sprite(400,100, 'windmill');
+	windmill.animations.add('spin');
+	windmill.animations.play('spin', 10, true);
 
 	//cow.animations.play('walk', 10, true); //3프레임까지는 움직이고 클릭했을때 4프레임부터 움직이게 
 }
