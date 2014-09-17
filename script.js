80c3006e083068624cb4f72399667ace9e1396b1
jujoo <wnfks93@gmail.com>
2014-09-17 18:48:31 +0900

    add cow2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
-var cow;
 var bg;
+var cow;
+var cow2
+
 
 function preload(){
 	game.load.spritesheet('cow', 'assets/cow.png', 100, 50);
+	game.load.spritesheet('cow', 'assets/cow2.png', 84, 45);
 	game.load.image('bg', 'assets/bg.jpg');
 }
 
@@ -15,9 +18,14 @@ function create(){
 	cow.inputEnabled = true;
 	cow.events.onInputDown.add(cowClick, this);
 
+	cow2 = game.add.sprite(100,100, 'cow2');
+	cow2.animations.add('walk');
+	cow2.inputEnabled = true;
+	cow2.events.onInputDown.add(cowClick, this);
 	//cow.animations.play('walk', 10, true); //3프레임까지는 움직이고 클릭했을때 4프레임부터 움직이게 
 }
 
 function cowClick() {
   	cow.animations.play('walk', 10, false);
+  	cow2.animations.play('walk', 10, false);
 }
