6e22e89854fa48b6e3e8fb6d85688b7568974205
jujoo <wnfks93@gmail.com>
2014-10-02 19:49:19 +0900

    cow 수정

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function create(){
 
 	cow = game.add.sprite(1948,640, 'cow');
 	cow.animations.add('eat', [0,1,2,3,4,5], 9, true, true );
-	cow.input.enableDrag(true);
+	//cow.input.enableDrag(true);
 	cow.walkAnim = cow.animations.add('walk',[6,7,8,9],9,true,true);
 	var walkCompleted = function(){
 		cow.animations.play('eat');
@@ -86,9 +86,9 @@ function cowClick() {
 
 // 	cow2.animations.play('walk', 10, false);
 }
-//function cowClick0() {
-//	cow.animations.play('walk',6,false);
-//}
+// function cowClick0() {
+// 	cow.animations.play('walk',6,false);
+// }
 function cowBurst(mouse) {
 	cowEmitter.start(true, 2000, null, 10);
 
